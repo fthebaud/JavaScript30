48e25e77476273b1e6b52dd17e8077730bbdddef
Fabien Thebaud <fthebaud@gmail.com>
2017-01-10 11:15:34 +0100

    Ex15 - Local storage

--- 15 - LocalStorage/script.js
+++ 15 - LocalStorage/script.js
@@ -2,11 +2,11 @@
 
 const addItems = document.querySelector('.add-items'); //form
 const itemsList = document.querySelector('.plates');
-const items = [];
+const items = JSON.parse(localStorage.getItem('items')) || [];
 
 function addItem(event) {
-  // par defaut lors d'un submit le formulaire recharge la page / envoie des données au serveur
-  // on veut empecher ça (we are going to do this client side)
+  // par defaut lors d'un submit le formulaire recharge la page / envoie des donnÃ©es au serveur
+  // on veut empecher Ã§a (we are going to do this client side)
   event.preventDefault();
   const text = this.querySelector('[name="item"]').value;
   const item = {
@@ -16,11 +16,35 @@ function addItem(event) {
   // clear the input : reset the form
   this.reset();
   items.push(item);
+  populateList(items, itemsList);
+  localStorage.setItem('items', JSON.stringify(items));
 }
 
-function populateList() {
-  // 7'0''
+function populateList(plates = [], domList) {
+  domList.innerHTML  = plates.map((plate, index) => {
+    return `
+      <li>
+        <input type="checkbox" id="item${index}" data-index=${index} ${plate.done ? 'checked' : ''}/>
+        <label for="item${index}">${plate.text}</label>
+      </li>
+    `;
+  }).join('');
 }
+
+function toggleDone(event) {
+  if (!event.target.matches('input')) return; //eslint-disable-line curly
+  const el = event.target;
+  const index = el.dataset.index;
+  items[index].done = !items[index].done;
+  localStorage.setItem('items', JSON.stringify(items));
+  // populateList(items, itemsList);
+}
+
 
 // we listen to 'submit', this event will be sent when there is a click on the submit button or enter
 addItems.addEventListener('submit', addItem);
+
+// event delegation : we listen on the list, not on its children
+itemsList.addEventListener('click', toggleDone);
+
+populateList(items, itemsList);
